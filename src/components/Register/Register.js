702810a40e00f359fb3e40948e55aa5acd7d7406
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-05 00:25:50 +0400

    register.js - constructor

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class Register extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: '',
+			name: ''
+		}
+	}
+	
 	render() {
 		return (
 		<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">

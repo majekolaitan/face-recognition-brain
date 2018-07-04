2a93d6a12223ad9dd4726917d285a1657b4bbdc3
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-04 23:20:12 +0400

    email and pass input onchange evt

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -32,11 +32,11 @@ class Signin extends React.Component {
 		      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
 		      <div className="mt3">
 		        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-		        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+		        <input onChange={this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
 		      </div>
 		      <div className="mv3">
 		        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-		        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+		        <input onChange={this.onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
 		      </div>
 		    </fieldset>
 		    <div className="">

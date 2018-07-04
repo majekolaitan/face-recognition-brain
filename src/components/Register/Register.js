f96d90510b2a61ba8f90d263957b2b32ceb74088
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-05 00:26:59 +0400

    register.js - events

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -9,6 +9,18 @@ class Register extends React.Component {
 			name: ''
 		}
 	}
+
+	onNameChange = (event) => {
+		this.setState({name: event.target.value})
+	}
+
+	onEmailChange = (event) => {
+		this.setState({email: event.target.value})
+	}
+
+	onPasswordChange = (event) => {
+		this.setState({password: event.target.value})
+	}
 	
 	render() {
 		return (

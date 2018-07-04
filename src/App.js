5cf58fa071c1a3efbb1985041d6e6a7732a82979
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-05 00:34:00 +0400

    user-profile data

--- src/App.js
+++ src/App.js
@@ -35,7 +35,24 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+          id: data.id,
+          name: data.name,
+          email: data.email,
+          entries: data.entries,
+          joined: data.joined
+        }})
   }
 
   calculateFaceLocation = (data) => {

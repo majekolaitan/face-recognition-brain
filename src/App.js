5e042535acc8c936511f56625d6987eee06f81f4
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-05 01:06:55 +0400

    user profile update 1

--- src/App.js
+++ src/App.js
@@ -81,7 +81,17 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .then(response => {
+      if (response) {
+          fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))})
     .catch(err => console.log(err));
   }
 

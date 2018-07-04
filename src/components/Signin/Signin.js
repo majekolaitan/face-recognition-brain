250c14403767b9fc405efe8f39c4fc08f2726cde
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-04 22:57:09 +0400

    make signin.js into class

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const Signin = ({onRouteChange}) => {
-	return (
+class Signin extends React.Component {
+	render() {
+		return (
 		<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
 		<main className="pa4 black-80">
 		  <div className="measure">
@@ -26,6 +27,7 @@ const Signin = ({onRouteChange}) => {
 		</main>
 		</article>
 		);
+	}
 }
 
 export default Signin;

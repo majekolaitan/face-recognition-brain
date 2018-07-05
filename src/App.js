7074bb2451e2e661cff618e4a490ecdf17e3126a
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-05 20:32:47 +0400

    moving clarifai to backend

--- src/App.js
+++ src/App.js
@@ -75,9 +75,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
+    
     .then(response => {
       if (response) {
           fetch('http://localhost:3000/image', {
